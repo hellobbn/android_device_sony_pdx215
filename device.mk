38b288560091f8034b4f79db03d66f4d73c77f1c
kyasu <kyasu.dev@gmail.com>
2023-01-01 15:17:58 +0900

    pdx215: overlay: Set physical location of side fps
    Change-Id: I7c65b393f20088f189c6d0e51ecae543fbf3f16a

--- device.mk
+++ device.mk
@@ -39,7 +39,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 PRODUCT_PACKAGES += \
-   SonyPDX215SystemUIRes
+    SonyPDX215SystemUIRes \
+    SonyPDX215FrameworksRes
 
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-lineage
